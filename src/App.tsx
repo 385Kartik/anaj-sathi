c0f8177eb4680f45bdfe7f2a82cd36d88e367a2b
Parmar Kartik Dinesh <kartikparmarcod@gmail.com>
2026-02-20 22:45:10 +0530

    added expense calculator route

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Reports from "./pages/Reports";
 import SettingsPage from "./pages/SettingsPage";
 import NotFound from "./pages/NotFound";
 import EditOrder from "./pages/EditOrder";
+import Expenses from "./pages/Expenses";
 
 const queryClient = new QueryClient();
 
@@ -33,6 +34,7 @@ const App = () => (
             <Route path="/drivers" element={<Drivers />} />
             <Route path="/reports" element={<Reports />} />
             <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/expenses" element={<Expenses />} />
             <Route path="/orders/edit/:id" element={<EditOrder />} />
           </Route>
           <Route path="*" element={<NotFound />} />
